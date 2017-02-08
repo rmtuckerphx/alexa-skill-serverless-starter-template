c5cc1d498e1ad7cbea664534c39b46afdb14fdb7
Mark Tucker <mark@shazaml.com>
2017-02-08 08:45:52 -0700

    Add functionality and tests for ListUtility

--- src/listUtility.js
+++ src/listUtility.js
@@ -1,15 +1,48 @@
 'use strict';
+const _ = require('lodash');
 
 // constructor
-function ListUtility(params) {
+function ListUtility(options) {
+
+    let defaults = {
+        sourceListSize: 0,
+        visitedIndexes: [],
+        resetWhenFull: true
+    };
+
     // always initialize all instance properties
-    this.sourceListSize = params.sourceListSize || 0;
-    this.visitedIndexes = params.visitedIndexes || [];
+    this.settings = Object.assign({}, defaults, options);
+    this._visitedIndexes = this.settings.visitedIndexes.slice(0); //clone array
 }
 
 // class methods
 ListUtility.prototype.getFirstIndex = function() {
-    return null;
+
+    const all = _.range(this.settings.sourceListSize);
+    let notVisited = _.difference(all, this._visitedIndexes);
+    let isVisitedFull = false;
+    let index = -1;
+
+    if (notVisited.length == 0) {
+        if (this.settings.resetWhenFull) {
+            this._visitedIndexes.length = 0; //clear array
+            notVisited = _.difference(all, this._visitedIndexes);
+        }
+        else {
+            isVisitedFull = true;
+        }
+    }
+
+    if (!isVisitedFull) {
+        index = _.head(notVisited);
+        this._visitedIndexes.push(index);
+    }
+
+    return {
+        index: index, 
+        newVisitedIndexes: this._visitedIndexes,
+        isVisitedFull: isVisitedFull
+    };
 };
 
 module.exports = ListUtility
--- test/unit/listUtilityTests.js
+++ test/unit/listUtilityTests.js
@@ -6,15 +6,151 @@ const assert = require( 'chai' ).assert;
 
 describe('listUtility', function () {
 
-    it('returns first index array when visitedIndexes is empty', function () {
-        let params = {
-            sourceListSize: 2,
-            visitedIndexes: []
+    it('should have default settings', function () {
+         let listUtility = new ListUtility();
+
+         assert.equal(0, listUtility.settings.sourceListSize);
+         assert.equal(0, listUtility.settings.visitedIndexes.length);
+
+    });
+
+    it('should use options for settings when passed', function () {
+        let options = {
+            sourceListSize: 3,
+            visitedIndexes: [1,2]
         };
 
-        let listUtility = new ListUtility(params);
+         let listUtility = new ListUtility(options);
+
+         assert.equal(3, listUtility.settings.sourceListSize);
+         assert.equal(2, listUtility.settings.visitedIndexes.length);
+
+    });
+
+    it('should return first index array when visitedIndexes is empty', function () {
+        let myIndexes = [];
+
+        let options = {
+            sourceListSize: 2,
+            visitedIndexes: myIndexes
+        };
+
+        let listUtility = new ListUtility(options);
         let result = listUtility.getFirstIndex();
 
-        assert.equal(1, result.index);
+        assert.equal(0, result.index);
+        assert.equal(1, result.newVisitedIndexes.length);
+        assert.deepEqual([0], result.newVisitedIndexes);
+        assert.deepEqual([], myIndexes);
+
     });
+
+    it('should return first index each call to getFirstIndex', function () {
+        let myIndexes = [];
+
+        let options = {
+            sourceListSize: 4,
+            visitedIndexes: myIndexes
+        };
+
+        let listUtility = new ListUtility(options);
+        let result = listUtility.getFirstIndex();
+        assert.equal(0, result.index);
+        assert.equal(1, result.newVisitedIndexes.length);
+        assert.deepEqual([0], result.newVisitedIndexes);
+        assert.deepEqual([], myIndexes);
+
+        result = listUtility.getFirstIndex();
+        assert.equal(1, result.index);
+        assert.equal(2, result.newVisitedIndexes.length);
+        assert.deepEqual([0,1], result.newVisitedIndexes);
+        assert.deepEqual([], myIndexes);
+        
+        result = listUtility.getFirstIndex();
+        assert.equal(2, result.index);
+        assert.equal(3, result.newVisitedIndexes.length);
+        assert.deepEqual([0,1,2], result.newVisitedIndexes);
+        assert.deepEqual([], myIndexes);
+
+        result = listUtility.getFirstIndex();
+        assert.equal(3, result.index);
+        assert.equal(4, result.newVisitedIndexes.length);
+        assert.deepEqual([0,1,2,3], result.newVisitedIndexes);
+        assert.deepEqual([], myIndexes);
+
+    });
+
+    it('should return next first index on call to getFirstIndex with ordered visitedIndexes', function () {
+        let myIndexes = [0,1];
+
+        let options = {
+            sourceListSize: 4,
+            visitedIndexes: myIndexes
+        };
+
+        let listUtility = new ListUtility(options);
+        let result = listUtility.getFirstIndex();
+        assert.equal(2, result.index);
+        assert.equal(3, result.newVisitedIndexes.length);
+        assert.deepEqual([0,1,2], result.newVisitedIndexes);
+        assert.deepEqual([0,1], myIndexes);
+    });
+
+    it('should return next first index on call to getFirstIndex with unordered visitedIndexes', function () {
+        let myIndexes = [3,0,2];
+
+        let options = {
+            sourceListSize: 4,
+            visitedIndexes: myIndexes
+        };
+
+        let listUtility = new ListUtility(options);
+        let result = listUtility.getFirstIndex();
+        assert.equal(1, result.index);
+        assert.equal(4, result.newVisitedIndexes.length);
+        assert.deepEqual([3,0,2,1], result.newVisitedIndexes);
+        assert.deepEqual([3,0,2], myIndexes);
+    });
+
+    it('should reset visitedIndexes when full and calling getLastIndex', function () {
+        let myIndexes = [0,1,2,3];
+
+        let options = {
+            sourceListSize: 4,
+            visitedIndexes: myIndexes
+        };
+
+        let listUtility = new ListUtility(options);
+        let result = listUtility.getFirstIndex();
+        assert.equal(0, result.index);
+        assert.equal(1, result.newVisitedIndexes.length);
+        assert.deepEqual([0], result.newVisitedIndexes);
+        assert.deepEqual([0,1,2,3], myIndexes);
+        assert.equal(false, result.isVisitedFull)
+    });
+
+    it('should not reset visitedIndexes when full and calling getLastIndex', function () {
+        let myIndexes = [0,1,2,3];
+
+        let options = {
+            sourceListSize: 4,
+            visitedIndexes: myIndexes,
+            resetWhenFull: false
+        };
+
+        let listUtility = new ListUtility(options);
+        let result = listUtility.getFirstIndex();
+        assert.equal(-1, result.index);
+        assert.equal(4, result.newVisitedIndexes.length);
+        assert.deepEqual([0,1,2,3], result.newVisitedIndexes);
+        assert.deepEqual([0,1,2,3], myIndexes);
+        assert.equal(true, result.isVisitedFull)
+    });
+
+    it('should return error when source list size is zero and calling getFirstIndex')
+    it('should return error when source list size is zero and calling getLastIndex')
+    it('should return error when source list size is zero and calling getRandomIndex')
+
+
+
 });
