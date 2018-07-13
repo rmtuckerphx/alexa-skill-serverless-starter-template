e2e201207d7f53a1fb1cfe7d2c4ed9eec8df3f94
Gareth Pelly <garethpelly@currencyfair.com>
2018-07-13 13:14:33 +0100

    Remove VoiceInsights analytics

--- src/handlers/defaultHandlers.js
+++ src/handlers/defaultHandlers.js
@@ -3,9 +3,7 @@
 
 console.log('Handler: defaultHandlers');
 
-const _ = require('lodash');
 const Config = require('../config/skill.config');
-//VI-REMOVE:const VoiceLabs = require('voicelabs')(Config.trackingToken);
 
 const defaultHandlers = {
 
@@ -18,11 +16,7 @@ const defaultHandlers = {
         this.attributes.speechOutput = ssmlResponse.speechOutput;
         this.attributes.repromptSpeech = ssmlResponse.reprompt;
 
-        //VI-REMOVE:console.log('LaunchRequest VoiceLabs.track');
-        //VI-REMOVE:VoiceLabs.track(this.event.session, 'LaunchRequest', null, ssmlResponse.speechOutput, (error, response) => {
-            //VI-REMOVE:console.log('error: ' + JSON.stringify(error, null, '  ') + '; response: ' + JSON.stringify(response, null, '  '));
-            this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt);
-        //VI-REMOVE:});
+        this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt);
     },
 
     'AMAZON.RepeatIntent': function () {
@@ -35,9 +29,7 @@ const defaultHandlers = {
             reprompt: this.attributes.repromptSpeech
         };
 
-        //VI-REMOVE:VoiceLabs.track(this.event.session, intent.name, null, ssmlResponse.speechOutput, (error, response) => {
-            this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt)
-        //VI-REMOVE:});
+        this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt)
     },
 
     'AMAZON.CancelIntent': function () {
@@ -60,9 +52,7 @@ const defaultHandlers = {
         this.attributes.speechOutput = " ";
         this.attributes.repromptSpeech = " ";
 
-        //VI-REMOVE:VoiceLabs.track(this.event.session, 'SessionEnd', null, ssmlResponse.speechOutput, (error, response) => {
-            this.emit(':tell', ssmlResponse.speechOutput); // :tell* or :saveState handler required here to save attributes to DynamoDB
-        //VI-REMOVE:});
+        this.emit(':tell', ssmlResponse.speechOutput); // :tell* or :saveState handler required here to save attributes to DynamoDB
     },
 
     'AMAZON.HelpIntent': function () {
@@ -75,9 +65,7 @@ const defaultHandlers = {
         this.attributes.speechOutput = ssmlResponse.speechOutput;
         this.attributes.repromptSpeech = ssmlResponse.reprompt;
 
-        //VI-REMOVE:VoiceLabs.track(this.event.session, intent.name, null, ssmlResponse.speechOutput, (error, response) => {
-            this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt);
-        //VI-REMOVE:});
+        this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt);
     },
     'Unhandled': function () {
         console.log('Unhandled');
@@ -87,9 +75,7 @@ const defaultHandlers = {
         this.attributes.speechOutput = ssmlResponse.speechOutput;
         this.attributes.repromptSpeech = ssmlResponse.reprompt;
 
-        //VI-REMOVE:VoiceLabs.track(this.event.session, 'Unhandled', null, ssmlResponse.speechOutput, (error, response) => {
-            this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt);
-        //VI-REMOVE:});
+        this.emit(':ask', ssmlResponse.speechOutput, ssmlResponse.reprompt);
     }
 };
 
